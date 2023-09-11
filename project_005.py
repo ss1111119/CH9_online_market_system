0d11b973d19486a472fa92b96e873dd3f161883f
steven <msycomtw@gmail.com>
2023-09-11 16:13:49 +0800

    initial setting

---
+++ project_005.py
@@ -0,0 +1,18 @@
+import json
+import math
+
+# 引入會員資料
+global user_data
+with open('user_data.json','r') as f:
+    user_data = json.load(f)
+    
+# 引入商品資料
+global product_list
+with open('product.json','r') as f:
+    product_list = json.load(f)
+
+global login_status
+login_status = False
+
+global cart
+cart = []
